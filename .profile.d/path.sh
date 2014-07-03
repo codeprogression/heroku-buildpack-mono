2d160d26669c4197b83dfbd3b9875039833c3c4f
codeprogression <richard@codeprogression.com>
2014-07-02 20:13:23 -0600

    Update path in the .profile.d/path.sh file

--- .profile.d/path.sh
+++ .profile.d/path.sh
@@ -1,5 +1,2 @@
-export PATH="$PATH:/app/mono/bin:/app/xsp/bin:/usr/local/bin:/usr/bin:/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib"
-
-DEPENDENCY_S3_BUCKET_PATH=${DEPENDENCY_S3_BUCKET_PATH:-"http://s3.amazonaws.com/mono-dependencies"}
-MONO_VERSION=${MONO_VERSION:-"3.2.8"}
+export PATH="$PATH:/app/vendor/mono/bin:/app/xsp/bin:/usr/local/bin:/usr/bin:/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/vendor/mono/lib"
