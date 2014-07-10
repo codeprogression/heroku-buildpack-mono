97af98d22d3109774369ed73dfb3658d12088fab
codeprogression <richard@codeprogression.com>
2014-07-10 00:04:53 -0600

    Update mono path

--- .profile.d/path.sh
+++ .profile.d/path.sh
@@ -1,2 +1,2 @@
-export PATH="$PATH:/app/vendor/mono/bin:/app/xsp/bin:/usr/local/bin:/usr/bin:/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/vendor/mono/lib"
+PATH="$PATH:$HOME/mono/bin:/app/xsp/bin:/usr/local/bin:/usr/bin:/bin"
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/mono/lib"
